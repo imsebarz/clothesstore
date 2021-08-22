e57c273b03d70bf7c8fe24863cee5e0f22ce5114
Sebastian Ruiz <sebastian.ruiz8@udea.edu.co>
2021-08-22 18:52:07 -0500

    Finished components design

---
+++ src/Components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/styles/components/loading.scss";
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <h3>Espera por favor</h3>
+    </div>
+  );
+};
+
+export default Loading;
